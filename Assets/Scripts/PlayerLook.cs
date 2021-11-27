c338906e672cc8d343ed48da6d1bed626b40be18
Sean Kovacs <sean.m.kovacs@gmail.com>
2021-11-27 16:24:40 -0600

    Player Movement, Camera Controller, Jump
    Basic Player Movement with Rigidbodies

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] float sensX;
+    [SerializeField] float sensY;
+
+    [SerializeField] Transform cam;
+    [SerializeField] Transform orientation;
+
+    private float mouseX;
+    private float mouseY;
+
+    private float multiplier = 1f;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    private void MyInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * multiplier;
+        xRotation -= mouseY * sensY * multiplier;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerHeight = 2f;
+
+    [SerializeField] Transform orientation;
+
+    public float moveSpeed = 6f;
+
+    [Header("Movement")]
+    private float horizontalMovement;
+    private float verticalMovement;
+    private float movementMultiplier = 7f;
+    // Air movement not needed for this project. Players move at the same speed if jumping.
+    //[SerializeField] float airMultiplier = 0.4f;
+    private float groundDrag = 6f;
+    private float airDrag = 2f;
+
+    private bool isGrounded;
+    public float jumpForce = 5f;
+
+    [Header("Ground Detection")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 moveDirection;
+    private Vector3 slopeDirection;
+
+    private Rigidbody rb;
+
+    private RaycastHit slopeHit;
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if(slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        MyInput();
+        ControlDrag();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        slopeDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+    }
+
+    private void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ControlDrag()
+    {
+        // Air drag not needed to replicate TF2 movement. Use if statement to check for grounded if adding air drag
+        rb.drag = groundDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // If using air drag, add air multiplier in if statement here
+        if (isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        
+    }
+}
