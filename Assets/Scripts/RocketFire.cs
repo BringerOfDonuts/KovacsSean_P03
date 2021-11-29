c725498885575d49d16e1ce3b49dc671493e41df
Sean Kovacs <sean.m.kovacs@gmail.com>
2021-11-28 21:37:44 -0600

    Rocket Launcher + Rocket Models, Rocket Movement, Basic Ammo System
    TODO: Add explosion force to rockets, track ammo in HUD, Add enemies, Visual Feedback

---
+++ Assets/Scripts/RocketFire.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketFire : MonoBehaviour
+{
+    private bool canFire = true;
+    private float fireDelay = 1f;
+    private int rocketsLoaded = 4;
+    private int rocketAmmo = 16;
+    private bool reloading = false;
+
+    [SerializeField] GameObject rocket;
+    [SerializeField] Transform barrel;
+    
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canFire)
+        {
+            fireDelay = 1f;
+            canFire = false;
+            rocketsLoaded -= 1;
+            FireRocket();
+            Debug.Log("Ammo Loaded: " + rocketsLoaded);
+            Debug.Log("Ammo in Reserves: " + rocketAmmo);
+        }
+
+        if (rocketsLoaded <= 0 && reloading == false)
+        {
+            canFire = false;
+            reloading = true;
+            InvokeRepeating("Reload", 1.0f, 1.0f);
+        }
+
+        if(fireDelay <= 0 && rocketsLoaded > 0)
+        {
+            // TODO: Maybe change to require full ammo before firing
+            canFire = true;
+        }
+
+        fireDelay = fireDelay - Time.deltaTime;
+    }
+
+    private void FireRocket()
+    {
+        Instantiate(rocket, barrel.position, barrel.rotation);
+    }
+
+    private void Reload()
+    {
+        // TODO: ADD RELOAD ANIMATION AND SOUNDS
+        if(rocketAmmo > 0)
+        {
+            rocketsLoaded += 1;
+            rocketAmmo -= 1;
+        }
+
+        if(rocketsLoaded == 4)
+        {
+            canFire = true;
+            reloading = false;
+            CancelInvoke();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    private float rocketSpeed = 10f;
+    private Vector3 explosionPos;
+
+    private float secondsUntilDestroy = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, secondsUntilDestroy);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * rocketSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // TODO: ADD EXPLOSION PARTICLES, SOUNDS, AND EXPLOSIVE FORCE TO PLAYER + ENEMIES
+        explosionPos = transform.position;
+        Debug.Log("Rocket had collided");
+        Destroy(gameObject);
+    }
+}
